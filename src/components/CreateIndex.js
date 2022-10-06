60c5ab7f3ca00ef0ef7283aaa6198c2600adb4db
Kishor Sarkar <kishor.sarkar@inadev.com>
2022-10-06 17:11:29 +0530

    SoulList Modified

---
+++ src/components/CreateIndex.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function CreateIndex(name,index) {
+  return (
+    <div>
+
+<ul class="list-group">
+  <li class="list-group-item">{name.name}</li>
+  <li class="list-group-item">Dapibus ac facilisis in</li>
+  <li class="list-group-item">Morbi leo risus</li>
+  <li class="list-group-item">Porta ac consectetur ac</li>
+  <li class="list-group-item">Vestibulum at eros</li>
+</ul>
+    </div>
+  )
+}
+
+export default CreateIndex
