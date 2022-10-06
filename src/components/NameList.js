a9888917d2f344dcd9a122ec0ce5024bba862464
Kishor Sarkar <kishor.sarkar@inadev.com>
2022-10-06 16:43:13 +0530

    Define Index

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -6,11 +6,13 @@ import Sports from "./Sports";
 
 function NameList() {
   // const GameList = Games.games.map((G) => <h2>{G}</h2>)
-  const soullist = data.eplTeams.epl_teams.map((S) => (
-    <Sports key={S.code} sports={S} />
+  const soullist = data.eplTeams.epl_teams.map((S,index) => ( 
+    <Sports key={index}  sports={S} />
+    
+   
   ));
   // console.log(Animals.animals);
-  console.log(data.eplTeams.epl_teams);
+  // console.log(index);
   return (
     <div>
       {/* <h2>{Games.games[0]}</h2>
