4fc69761c658a7ea725174ae20c608886b5c9bd2
Kishor Sarkar <kishor.sarkar@inadev.com>
2022-10-15 19:15:53 +0530

    update code

--- src/components/WithCounter.js
+++ src/components/WithCounter.js
@@ -2,21 +2,25 @@ import React from "react";
 const UpdateComponents = (OriginalComponents) => {
   class NewComponent extends React.Component {
     constructor(props) {
-        super(props)
-      
-        this.state = {
-           count:0
-        }
-        
-      }
-      incrementCount = ()=>{
-          this.setState(prevState =>{
-              // console.log(count : prevState.count+1);
-              return {count : prevState.count+1}
-          })
-      }
+      super(props);
+
+      this.state = {
+        count: 0,
+      };
+    }
+    incrementCount = () => {
+      this.setState((prevState) => {
+        // console.log(count : prevState.count+1);
+        return { count: prevState.count + 1 };
+      });
+    };
     render() {
-      return <OriginalComponents count={this.state.count} incrementCount={this.incrementCount}></OriginalComponents>;
+      return (
+        <OriginalComponents
+          count={this.state.count}
+          incrementCount={this.incrementCount}
+        ></OriginalComponents>
+      );
     }
   }
   return NewComponent;
